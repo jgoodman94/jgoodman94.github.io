1cc7a264ee4ec71c857f5f50711511546269a07d
jgoodman94 <jesse@Jesses-MacBook-Pro.local>
2015-01-01 16:43:36 -0500

    testing

--- js/indexjs.js
+++ js/indexjs.js
@@ -142,7 +142,7 @@ function rageQuit()
 {
 	// don't delete an entry if we didnt just create one!
 	if (id == "undefined")
-		return "IT WAS UNDEFINED";
+		return "";
 
 	// if we're looking for a partner and then quit
 	// search database for our id and delete the request
