49639209c7ae86c055a4f632fc992158c02a77ca
jgoodman94 <jesse@Jesses-MBP.home>
2015-01-01 20:16:46 -0500

    set looking to false

--- js/indexjs.js
+++ js/indexjs.js
@@ -27,6 +27,7 @@ $(function() {
     // click "go", aka 'I'm ready to chat!'
     $('#start').click(function() {
     	giveUp = window.setTimeout(function() {
+    		looking = false;
     		$('#remoteVid').html('');
     		$('#disconnected').html('Everyone\'s busy in their chats, or very few people are online at the moment.<br>Try again?');
     		$('#disconnected').show();
