f4a596923c1c64cbac4d32837bd6bfa5ee05e8bc
jgoodman94 <jesse@Jesses-MBP.home>
2015-01-02 00:59:39 -0500

    final working copy of text chat
    …hopefully

--- js/indexjs.js
+++ js/indexjs.js
@@ -1,35 +1,17 @@
 $(function() {
-	//alert('it has been changed');
-	vchatCheck();
+    alert('it has been changed');
+    vchatCheck();
     //set chat-output size to rest of screen
     window.addEventListener("resize", calcOutputHeight);
 
     function calcOutputHeight() {
-    	var h = $(window).height();
-    	var leftover = h - $('#header').height() - $('#localVid').height() - $('#chat-input-row').height();
+        var h = $(window).height();
+        var leftover = h - $('#header').height() - $('#localVid').height() - $('#chat-input-row').height();
         //console.log(leftover);
         $("#chat-output").height(leftover);
         $("#chat-output-row").height(leftover);
     };
     channel = new DataChannel();
-
-    channel.onopen = function() {
-    	$chatInput.disabled = false;
-                    // Tinder like prompting messages here
-                    $chatOutput.append('Say hi!<br>');
-                    $chatInput.focus();
-                };
-
-                channel.onmessage = function(message) {
-                	$chatOutput.append('<span style="color:#fac03b"><b>Stranger:</b></span> ' + message + '<br />');
-                	console.log('LOOK AT THIS MESSAGE: ' + message);
-                	chatOutput.scrollTop = chatOutput.scrollHeight;
-                };
-
-                channel.onleave = function(userid) {
-                    //$chatOutput.innerHTML = userid + ' Left.<hr />' + $chatOutput.innerHTML;
-                };
-
 
     //TEXT FUNCTIONS
     $chatOutput = $('#chat-output');
@@ -39,11 +21,11 @@ $(function() {
     var chatOutput = document.getElementById('chat-output');
 
     $chatInput.keyup(function(e) {
-    	if (e.keyCode != 13) return;
-    	channel.send(this.value);
-    	$chatOutput.append('<span style="color:#4099FF"><b>Me</b>:</span> ' + this.value + '<br />');
-    	chatOutput.scrollTop = chatOutput.scrollHeight;
-    	this.value = '';
+        if (e.keyCode != 13) return;
+        channel.send(this.value);
+        $chatOutput.append('<span style="color:#4099FF"><b>Me</b>:</span> ' + this.value + '<br />');
+        chatOutput.scrollTop = chatOutput.scrollHeight;
+        this.value = '';
     });
 
 
@@ -72,19 +54,19 @@ $(function() {
 
     // click "go", aka 'I'm ready to chat!'
     $('#start').click(function() {
-    	looking = true;
-    	giveUpIn(10000);
-    	channel = new DataChannel();
-    	$('#childTop').html('');
-    	$('#childFoot').html('');
+        looking = true;
+        giveUpIn(10000);
+        channel = new DataChannel();
+        $('#childTop').html('');
+        $('#childFoot').html('');
         // look for some frands, join if there
         searchRequest(webrtc);
     });
 
     // get rid of spinner when friend comes on
     webrtc.on('peerStreamAdded', function() {
-    	window.clearTimeout(giveUp);
-    	looking = false;
+        window.clearTimeout(giveUp);
+        looking = false;
         //id = "undefined";
         $('#disconnected').hide();
         $('#doggy').hide();
@@ -123,7 +105,7 @@ $(function() {
     });
 */
 
-$('#next').click(function() {
+    $('#next').click(function() {
         // leave rooms when u click next
         webrtc.leaveRoom();
         channel.leave();
@@ -138,8 +120,8 @@ $('#next').click(function() {
         $('#next').hide();
         // reset text in disconnected
         $('#disconnected').html('Your partner has disconnected :(<br><br>');
-        	searchRequest(webrtc);
-        });
+        searchRequest(webrtc);
+    });
 
 
     //add effects to video
@@ -148,12 +130,12 @@ $('#next').click(function() {
 
     //change the current effect
     function changeFilter(e) {
-    	var la = e.target;
-    	la.className = '';
-    	var effect = filters[index++ % filters.length];
+        var la = e.target;
+        la.className = '';
+        var effect = filters[index++ % filters.length];
         // loop through filters.
         if (effect) {
-        	la.classList.add(effect);
+            la.classList.add(effect);
         }
     }
 
@@ -170,38 +152,55 @@ $('#next').click(function() {
 
 // add request for chat to database
 function addRequest(webrtc) {
-	var Request = Parse.Object.extend("Request");
-	var request = new Request();
-	request.save(null, {
-		success: function(request) {
-			id = request.id;
-			console.log("Request added under id: " + id);
+    var Request = Parse.Object.extend("Request");
+    var request = new Request();
+    request.save(null, {
+        success: function(request) {
+            id = request.id;
+            console.log("Request added under id: " + id);
             //create webrtc room and text chat room
             webrtc.joinRoom(id);
             channel.open(id);
 
 
+            // channel handlers
+            channel.onopen = function() {
+                $chatInput.disabled = false;
+                // Tinder like prompting messages here
+                $chatOutput.append('<b>Say hi!</b><br>');
+                $chatInput.focus();
+            };
+
+            channel.onmessage = function(message) {
+                $chatOutput.append('<span style="color:#fac03b"><b>Stranger:</b></span> ' + message + '<br />');
+                console.log('LOOK AT THIS MESSAGE: ' + message);
+                chatOutput.scrollTop = chatOutput.scrollHeight;
+            };
+
+            channel.onleave = function(userid) {
+                //$chatOutput.innerHTML = userid + ' Left.<hr />' + $chatOutput.innerHTML;
+            };
 
 
         },
         error: function() {
-        	console.log("something's fucked up");
+            console.log("something's fucked up");
         }
     });
 }
 
 // returns either request id or undefined
 function searchRequest(webrtc) {
-	$('.spinner').show();
-	var Request = Parse.Object.extend("Request");
-	var requestQuery = new Parse.Query(Request);
-	requestQuery.first({
-		success: function(request) {
+    $('.spinner').show();
+    var Request = Parse.Object.extend("Request");
+    var requestQuery = new Parse.Query(Request);
+    requestQuery.first({
+        success: function(request) {
             // entry is in database
             // join room of other person, then destroy that persons
             if (request != undefined) {
-            	textID = request.id;
-            	console.log("matched to: " + request.id);
+                textID = request.id;
+                console.log("matched to: " + request.id);
 
 
                 // join webrtc and text chat rooms
@@ -209,7 +208,23 @@ function searchRequest(webrtc) {
                 channel.connect(request.id);
 
 
+                // channel handlers
+                channel.onopen = function() {
+                    $chatInput.disabled = false;
+                    // Tinder like prompting messages here
+                    $chatOutput.append('Say hi!<br>');
+                    $chatInput.focus();
+                };
 
+                channel.onmessage = function(message) {
+                    $chatOutput.append('<span style="color:#fac03b"><b>Stranger:</b></span> ' + message + '<br />');
+                    console.log('LOOK AT THIS MESSAGE: ' + message);
+                    chatOutput.scrollTop = chatOutput.scrollHeight;
+                };
+
+                channel.onleave = function(userid) {
+                    //$chatOutput.innerHTML = userid + ' Left.<hr />' + $chatOutput.innerHTML;
+                };
 
 
                 destroyPartner(request.id);
@@ -222,45 +237,45 @@ function searchRequest(webrtc) {
             return;
         },
         error: function(error) {
-        	alert("Error: " + error.code + " " + error.message);
+            alert("Error: " + error.code + " " + error.message);
         }
     });
 }
 
 
 function destroyPartner(searchResult) {
-	var Request = Parse.Object.extend("Request");
-	var requestQuery = new Parse.Query(Request);
-	requestQuery.equalTo("objectId", searchResult);
-	requestQuery.first({
-		success: function(searchResult) {
-			if (searchResult != undefined) {
-				searchResult.destroy({
-					success: function(searchResult) {
-						console.log("deleted request object with id: " + searchResult.id);
-					},
-					error: function(searchResult, error) {
-						alert("Error. Object could not be deleted: " + error.code + " " + error.message);
-					}
-				});
-			}
-		},
-		error: function(error) {
-			alert("Error. Request not found: " + error.code + " " + error.message);
-		}
-	});
+    var Request = Parse.Object.extend("Request");
+    var requestQuery = new Parse.Query(Request);
+    requestQuery.equalTo("objectId", searchResult);
+    requestQuery.first({
+        success: function(searchResult) {
+            if (searchResult != undefined) {
+                searchResult.destroy({
+                    success: function(searchResult) {
+                        console.log("deleted request object with id: " + searchResult.id);
+                    },
+                    error: function(searchResult, error) {
+                        alert("Error. Object could not be deleted: " + error.code + " " + error.message);
+                    }
+                });
+            }
+        },
+        error: function(error) {
+            alert("Error. Request not found: " + error.code + " " + error.message);
+        }
+    });
 }
 
 function giveUpIn(numMillis) {
-	giveUp = window.setTimeout(function() {
-		looking = false;
-		$('.spinner').hide();
-		$('#disconnected').html('Everyone\'s busy in their chats,<br> or very few people are online at the moment.');
-		$('#disconnected').show();
-		$('#doggy').show();
-		$('#next').show();
-		destroyPartner(id);
-	}, numMillis);
+    giveUp = window.setTimeout(function() {
+        looking = false;
+        $('.spinner').hide();
+        $('#disconnected').html('Everyone\'s busy in their chats,<br> or very few people are online at the moment.');
+        $('#disconnected').show();
+        $('#doggy').show();
+        $('#next').show();
+        destroyPartner(id);
+    }, numMillis);
 }
 
 function rageQuit() {
@@ -269,56 +284,56 @@ function rageQuit() {
         var Request = Parse.Object.extend("Request");
         var query = new Parse.Query(Request);
         query.get(id, {
-        	success: function(myObj) {
-        		myObj.destroy({});
-        	},
-        	error: function() {
+            success: function(myObj) {
+                myObj.destroy({});
+            },
+            error: function() {
                 // parse error with error code
             }
         });
         return "There'll be more people on later, we promise.";
     }
     // reset things on leaving page
-    window.onbeforeunload = function(e) {
-    	if (looking == true)
-    		return rageQuit();
+window.onbeforeunload = function(e) {
+    if (looking == true)
+        return rageQuit();
 
-    	return;
-    };
+    return;
+};
 
 
 
 
 // check if browser will support vchat
 function vchatCheck() {
-	var alertMessage = 'This is a pretty cool site.<br>';
+    var alertMessage = 'This is a pretty cool site.<br>';
 
-	if (bowser.name == 'Chrome') {
-		if (bowser.version < 23) {
-			alertMessage += '<span style=color:white>Please update Chrome if you want to experience it.</span><br><br>Then we\'ll let you in.';
-			$('#modal').html(alertMessage);
-			$('#overlay').show();
-			$('#modal').show();
-		}
-	} else if (bowser.name == 'Firefox') {
-		if (bowser.version < 22) {
+    if (bowser.name == 'Chrome') {
+        if (bowser.version < 23) {
+            alertMessage += '<span style=color:white>Please update Chrome if you want to experience it.</span><br><br>Then we\'ll let you in.';
+            $('#modal').html(alertMessage);
+            $('#overlay').show();
+            $('#modal').show();
+        }
+    } else if (bowser.name == 'Firefox') {
+        if (bowser.version < 22) {
 
-			alertMessage += '<span style="color:white">Please update Firefox if you want to experience it.</span><br><br>Then we\'ll let you in.';
-			$('#modal').html(alertMessage);
-			$('#overlay').show();
-			$('#modal').show();
-		}
-	} else if (bowser.name == 'Opera') {
-		if (bowser.version < 18) {
-			alertMessage += '<span style="color:white">Please update Opera if you want to experience it.</span><br><br>Then we\'ll let you in.';
-			$('#modal').html(alertMessage);
-			$('#overlay').show();
-			$('#modal').show();
-		}
-	} else {
-		alertMessage += '<span style="color:white">Please download Chrome or Firefox if you want to experience it.</span><br><br>Then we\'ll let you in.';
-		$('#modal').html(alertMessage);
-		$('#overlay').show();
-		$('#modal').show();
-	}
+            alertMessage += '<span style="color:white">Please update Firefox if you want to experience it.</span><br><br>Then we\'ll let you in.';
+            $('#modal').html(alertMessage);
+            $('#overlay').show();
+            $('#modal').show();
+        }
+    } else if (bowser.name == 'Opera') {
+        if (bowser.version < 18) {
+            alertMessage += '<span style="color:white">Please update Opera if you want to experience it.</span><br><br>Then we\'ll let you in.';
+            $('#modal').html(alertMessage);
+            $('#overlay').show();
+            $('#modal').show();
+        }
+    } else {
+        alertMessage += '<span style="color:white">Please download Chrome or Firefox if you want to experience it.</span><br><br>Then we\'ll let you in.';
+        $('#modal').html(alertMessage);
+        $('#overlay').show();
+        $('#modal').show();
+    }
 }
