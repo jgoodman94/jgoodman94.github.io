1c86c94b214511225e7e18f78c460985f90d6914
jgoodman94 <jesse@Jesses-MacBook-Pro.local>
2015-01-02 04:19:30 -0500

    please

--- js/indexjs.js
+++ js/indexjs.js
@@ -1,5 +1,5 @@
 $(function() {
-	alert('it has been changed');
+	//alert('it has been changed');
 	vchatCheck();
     //set chat-output size to rest of screen
     window.addEventListener("resize", calcOutputHeight);
@@ -290,6 +290,7 @@ function giveUpIn(numMillis) {
 		$('#doggy').show();
 		$('#bigShuffle').show();
 		$('#smallShuffle').show();
+		$('#smallShuffle').css('color','4099FF');
 		destroyPartner(id);
 	}, numMillis);
 }
