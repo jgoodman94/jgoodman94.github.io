c5849318646ea7662b7173525a8f0f52c66c57b2
jgoodman94 <jesse@Jesses-MacBook-Pro.local>
2015-01-02 04:16:40 -0500

    cosmetic changes

--- js/indexjs.js
+++ js/indexjs.js
@@ -1,5 +1,5 @@
 $(function() {
-	alert('it has been changed');
+	//alert('it has been changed');
 	vchatCheck();
     //set chat-output size to rest of screen
     window.addEventListener("resize", calcOutputHeight);
@@ -177,6 +177,7 @@ function addRequest(webrtc) {
             // channel handlers
             channel.onopen = function() {
             	$('.spinner').hide();
+            	$('#smallShuffle').css('color','4099FF');
             	//$chatInput.disabled = false;
                 // Tinder like prompting messages here
                // $chatOutput.append('<b>Say hi!</b><br>');
@@ -223,6 +224,7 @@ function searchRequest(webrtc) {
                 // channel handlers
                 channel.onopen = function() {
                 	$('.spinner').hide();
+                	$('#smallShuffle').css('color','4099FF');
                 	//chatInput.disabled = false;
                     // Tinder like prompting messages here
                   //  $chatOutput.append('<b>Say hi!</b><br>');
