82d6811d01274a196533499e2fb4c663b228dad9
jgoodman94 <jesse@Jesses-MBP.home>
2015-01-27 01:25:45 -0500

    retesting

--- js/indexjs.js
+++ js/indexjs.js
@@ -2,6 +2,15 @@
 
   $(function() {
 
+      var peer = new Peer({
+          key: 'lwjd5qra8257b9'
+      }); // this is me
+      peer.on('open', function(id) {
+          peerID = id;
+          console.log('My peer ID is: ' + id);
+      });
+
+
       // initialize with API key
       Parse.initialize("cLQ1TweezsDIp2ysSvYvXETLozVZIMdRfExqEg7u", "fgapofWIKhtAQfuToqAbRRlNHCAfBbFR6pusDzBk");
 
@@ -111,7 +120,7 @@
       /* deal with color changes
       webrtc.on('sepia', function(data) {
           console.log('sepia sent?');
-      })*/
+        })*/
 
       /*
     $('#localVid').click(function() {
@@ -128,10 +137,10 @@
     });
   */
 
-  $('.next').click(function() {
-      if (looking == true)
-          return;
-      $('#smallShuffle').css('color', 'lightgrey');
+      $('.next').click(function() {
+          if (looking == true)
+              return;
+          $('#smallShuffle').css('color', 'lightgrey');
           // leave rooms when u click next
           webrtc.leaveRoom();
           // channel.leave();
@@ -145,8 +154,8 @@
           $('#bigShuffle').hide();
           // reset text in disconnected
           $('#disconnected').html('Your partner has disconnected :(<br><br>');
-              searchRequest(webrtc);
-          });
+          searchRequest(webrtc);
+      });
 
 
       //add effects to video
@@ -171,21 +180,22 @@
       /*send filters to other person
               $('#localVid').click(function(peer) {
                   peer.send('sepia');
-              });*/
+                });*/
 
-});
+  });
 
   // add request for chat to database
   function addRequest(webrtc) {
       var Request = Parse.Object.extend("Request");
       var request = new Request();
+      request.set("peerID", peerID);
       request.save(null, {
           success: function(request) {
-              id = request.id;
-              console.log("Request added under id: " + id);
+              myID = request.id;
+              console.log("Request added under id: " + myID);
               //create webrtc room and text chat room
-              webrtc.joinRoom(id);
-              channel.open(id);
+              webrtc.joinRoom(myID);
+              channel.open(myID);
 
 
               // channel handlers
@@ -207,7 +217,7 @@
                   alert('new person here!');
                }
                */
-               channel.onleave = function() {
+              channel.onleave = function() {
                   channel = new DataChannel();
                   //$chatInput.disabled = true;
                   //$chatOutput.innerHTML = userid + ' Left.<hr />' + $chatOutput.innerHTML;
@@ -219,7 +229,7 @@
               console.log("something's fucked up");
           }
       });
-}
+  }
 
   // returns either request id or undefined
   function searchRequest(webrtc) {
@@ -238,6 +248,7 @@
                   // join webrtc and text chat rooms
                   webrtc.joinRoom(request.id);
                   channel.connect(request.id);
+                  console.log('dest peer id is ' + request.attributes.peerID);
 
 
                   // channel handlers
@@ -275,52 +286,52 @@
               alert("Error: " + error.code + " " + error.message);
           }
       });
-}
+  }
 
 
-function destroyPartner(searchResult) {
-  var Request = Parse.Object.extend("Request");
-  var requestQuery = new Parse.Query(Request);
-  requestQuery.equalTo("objectId", searchResult);
-  requestQuery.first({
-      success: function(searchResult) {
-          if (searchResult != undefined) {
-              searchResult.destroy({
-                  success: function(searchResult) {
-                      console.log("deleted request object with id: " + searchResult.id);
-                  },
-                  error: function(searchResult, error) {
-                      alert("Error. Object could not be deleted: " + error.code + " " + error.message);
-                  }
-              });
+  function destroyPartner(searchResult) {
+      var Request = Parse.Object.extend("Request");
+      var requestQuery = new Parse.Query(Request);
+      requestQuery.equalTo("objectId", searchResult);
+      requestQuery.first({
+          success: function(searchResult) {
+              if (searchResult != undefined) {
+                  searchResult.destroy({
+                      success: function(searchResult) {
+                          console.log("deleted request object with id: " + searchResult.id);
+                      },
+                      error: function(searchResult, error) {
+                          alert("Error. Object could not be deleted: " + error.code + " " + error.message);
+                      }
+                  });
+              }
+          },
+          error: function(error) {
+              alert("Error. Request not found: " + error.code + " " + error.message);
           }
-      },
-      error: function(error) {
-          alert("Error. Request not found: " + error.code + " " + error.message);
-      }
-  });
-}
+      });
+  }
 
-function giveUpIn(numMillis) {
-  giveUp = window.setTimeout(function() {
-      looking = false;
-      $('.spinner').hide();
-      $('#disconnected').html('Everyone\'s busy in their chats,<br> or very few people are online at the moment.');
-      $('#disconnected').show();
-      $('#doggy').show();
-      $('#bigShuffle').show();
+  function giveUpIn(numMillis) {
+      giveUp = window.setTimeout(function() {
+          looking = false;
+          $('.spinner').hide();
+          $('#disconnected').html('Everyone\'s busy in their chats,<br> or very few people are online at the moment.');
+          $('#disconnected').show();
+          $('#doggy').show();
+          $('#bigShuffle').show();
           //$('#smallShuffle').show();
           $('#smallShuffle').css('color', '#4099FF');
-          destroyPartner(id);
+          destroyPartner(myID);
       }, numMillis);
-}
+  }
 
-function rageQuit() {
+  function rageQuit() {
           // if we're looking for a partner and then quit
           // search database for our id and delete the request
           var Request = Parse.Object.extend("Request");
           var query = new Parse.Query(Request);
-          query.get(id, {
+          query.get(myID, {
               success: function(myObj) {
                   myObj.destroy({});
               },
@@ -331,69 +342,69 @@ function rageQuit() {
           return "There'll be more people on later, we promise.";
       }
       // reset things on leaving page
-      window.onbeforeunload = function(e) {
-          window.open("http://www.w3schools.com");
+  window.onbeforeunload = function(e) {
+      window.open("http://www.w3schools.com");
 
-          if (looking == true)
-              return rageQuit();
+      if (looking == true)
+          return rageQuit();
 
-          return;
-      };
+      return;
+  };
 
-      //calculate and display users online
-      function getUsersOnline() {
-        // if taking too long or if this is too intense on the
-        // server, change this code to work with a forever lasting
-        // row that keeps a count of users online. then, just
-        // retrieve this value instead of counting up the whole table.
-        // increment and decrement this value on page load and
-        // when we find a user that has a dead heartbeat, respectively.
+  //calculate and display users online
+  function getUsersOnline() {
+          // if taking too long or if this is too intense on the
+          // server, change this code to work with a forever lasting
+          // row that keeps a count of users online. then, just
+          // retrieve this value instead of counting up the whole table.
+          // increment and decrement this value on page load and
+          // when we find a user that has a dead heartbeat, respectively.
 
-        var Online = Parse.Object.extend("Online");
-        var query = new Parse.Query(Online);
+          var Online = Parse.Object.extend("Online");
+          var query = new Parse.Query(Online);
 
-        query.count({
-          success: function(number) {
-              if (number == 1)
-                  $('#usersOnline').text(number + " user online");
-              else
-                  $('#usersOnline').text(number + " users online");
-          },
-          error: function(object, error) {
-              //error somehow
-              $('#usersOnline').text("Unlimited users online");
-          }
-      });
+          query.count({
+              success: function(number) {
+                  if (number == 1)
+                      $('#usersOnline').text(number + " user online");
+                  else
+                      $('#usersOnline').text(number + " users online");
+              },
+              error: function(object, error) {
+                  //error somehow
+                  $('#usersOnline').text("Unlimited users online");
+              }
+          });
 
-    }
-/*
-    function decrementUsers() {
-      //decrease users online
-      // Create a pointer to an object of class Point with id dlkj83d
-      var Point = Parse.Object.extend("Online");
-      var point = new Point();
-      point.id = "sNus417KBq";
+      }
+      /*
+          function decrementUsers() {
+            //decrease users online
+            // Create a pointer to an object of class Point with id dlkj83d
+            var Point = Parse.Object.extend("Online");
+            var point = new Point();
+            point.id = "sNus417KBq";
 
-      // decrement onleave
-      point.increment("usersOnline", -1);
+            // decrement onleave
+            point.increment("usersOnline", -1);
 
-      // Save
-      point.save(null, {
-          success: function(point) {
-              // Saved successfully.
-          },
-          error: function(point, error) {
-              // The save failed.
-              // error is a Parse.Error with an error code and description.
-          }
-      });
+            // Save
+            point.save(null, {
+                success: function(point) {
+                    // Saved successfully.
+                },
+                error: function(point, error) {
+                    // The save failed.
+                    // error is a Parse.Error with an error code and description.
+                }
+            });
 
-}*/
+      }*/
 
- //add user to database, give him a heartbeat
- // and check/delete any users whose heartbeat
- // has not been updated in the past 10 minutes
-function initiateUser() {
+  //add user to database, give him a heartbeat
+  // and check/delete any users whose heartbeat
+  // has not been updated in the past 10 minutes
+  function initiateUser() {
 
       var User = Parse.Object.extend("Online");
       var user = new User();
@@ -431,32 +442,32 @@ function initiateUser() {
 
   //deletes inactive users from database
   function deleteDeadUsers() {
-    var usersOnline = Parse.Object.extend("Online");
-    var query = new Parse.Query(usersOnline);
+      var usersOnline = Parse.Object.extend("Online");
+      var query = new Parse.Query(usersOnline);
 
 
-    query.find({
-        success: function(results) {
-            currentDate = new Date();
-            resultLength = results.length;
+      query.find({
+          success: function(results) {
+              currentDate = new Date();
+              resultLength = results.length;
 
-            for (var i = 0; i < resultLength; i++) {
-                var updatedTime = results[i].updatedAt.getTime();
-                var timeSinceUpdate = Math.floor((currentDate.getTime() - updatedTime) / 1000);
+              for (var i = 0; i < resultLength; i++) {
+                  var updatedTime = results[i].updatedAt.getTime();
+                  var timeSinceUpdate = Math.floor((currentDate.getTime() - updatedTime) / 1000);
 
-                if (timeSinceUpdate > (HEARTBEAT_TIME/1000))
-                    results[i].destroy({});
-            }
+                  if (timeSinceUpdate > (HEARTBEAT_TIME / 1000))
+                      results[i].destroy({});
+              }
 
-            getUsersOnline();
-        },
-        error: function(error) {
-            // some error
-        }
-    });
+              getUsersOnline();
+          },
+          error: function(error) {
+              // some error
+          }
+      });
 
 
-}
+  }
 
 
   // check if browser will support vchat
